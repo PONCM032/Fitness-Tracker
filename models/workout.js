da9df9ab7fb204e06a8e756f0babb2d1e16e3a51
Melanie <melanie-ponce@hotmail.com>
2020-07-25 16:56:15 -0400

    complete workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,37 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-   day: {
+  day: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   exercises: [
-      {
-        type: {
-            type: String,
-            trim: true,
-            required: "Please select a type of workout for your session."
-        },
-        name:{
-            type: String,
-            trim: true,
-            required: "Please state the name of your workout."
-        },
-        duration:{
-            type: Number,
-            required: "Please enter the duration of your workout."
-        },
-      }
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please select a type of workout for your session.",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please state the name of your workout.",
+      },
+      duration: {
+        type: Number,
+        required: "Please enter the duration of your workout.",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
   ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
-
-
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-
-    // distance: 4
