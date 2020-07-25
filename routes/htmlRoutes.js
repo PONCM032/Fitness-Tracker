181b30b75cbe52f8fc712bf50d12be5b8fb9c650
Melanie <melanie-ponce@hotmail.com>
2020-07-25 19:58:16 -0400

    complete htmlroutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,16 @@
-const model = require("../models");
-// "/api/workouts"
+const path = require("path");
+const router = require("express").Router();
 
-// "/api/workouts/range"
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
 
-// "/"
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
+
+module.exports = router;
