a12b53ac23d358d661209f0f19e5e57e3837a0ef
Melanie <melanie-ponce@hotmail.com>
2020-07-25 16:53:42 -0400

    add starter code to workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,21 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
+   day: {
     type: Date,
     default: Date.now
-  }
+  },
+  exercises: [
+      {
+        type: {
+            type: String,
+            trim: true,
+            required: "Please select a type of workout for your session."
+        },
+        name:{
+            type: String,
+            trim: true,
+            required: "Please state the name of your workout."
+        },
+        duration:{
+            type: Number,
+            required: "Please enter the duration of your workout."
+        },
+      }
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
+
+
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+
+    // distance: 4
