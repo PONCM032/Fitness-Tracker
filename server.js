0878e9db66819e55201a7d4b1380386796905723
Melanie <melanie-ponce@hotmail.com>
2020-07-25 20:29:30 -0400

    update heroku link

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //require
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const logger = require("morgan");
 const app = express();
 
@@ -13,11 +13,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://Melanie:pass123@ds253713.mlab.com:53713/heroku_zpszr500",
+  {
+    useMongoClient: true,
+  }
+);
 
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
